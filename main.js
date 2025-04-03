28906edae9e5e702c3014fecf33c8ab9b294732c
sparshgoyal199 <sparshgoyal199@gmail.com>
2025-04-03 14:45:28 +0530

    ngRock url added

--- main.js
+++ main.js
@@ -4,39 +4,11 @@ let mock_data = {
     "description":""
 };
 
-function fun1(){
-    fetch('http://127.0.0.1:8080/start_emulator',{
-        method:'POST',
-        //body:forms
-    })
-    .then(res => {
-        if (!res.ok) {
-            //res.text is itself a promise response is the result of promise
-            return res.text().then(response => {
-                throw new Error(response.substring(11,response.length-2))
-                //response.substring for customising the format of error send by the backend
-            })  
-        }
-        //we can also show the customise error message into frontend
-        return res.json()
-        //first of all convert the content of backend into json format
-    })
-    .then(data =>{  
-        console.log(data);
-        
-        //here we cant write swal function as it is asynchr code(which runs always in last) and our page is loading automatically page loading prevent running of async code that's why simple alert here
-    })
-    .catch(e => {
-        swal.fire({
-            icon:"error",
-            text: `${e}`,
-            className: "sweetBox"
-          })
-    })
-}
-
 function fun2(){
-    fetch('http://127.0.0.1:8080/get_system_info').
+    fetch('https://3844-106-219-164-121.ngrok-free.app/get_system_info',{
+    headers: {
+        "ngrok-skip-browser-warning": "true"
+    }}).
     then(res => {
         if (!res.ok) {
             return res.text().then(response => {
@@ -60,9 +32,9 @@ function fun2(){
 }
 
 function fun3(){
-    fetch('http://127.0.0.1:8080/app',{
+    fetch('https://3844-106-219-164-121.ngrok-free.app/app',{
         method:'POST',
-        headers:{"Content-type":"application/json"},
+        headers:{"Content-type":"application/json","ngrok-skip-browser-warning": "true"},
         body:JSON.stringify(mock_data)
     })
     .then(res => {
@@ -77,10 +49,9 @@ function fun3(){
         return res.json()
         //first of all convert the content of backend into json format
     })
-    .then(data =>{  
+    .then(data =>{   
         console.log(data);
         
-        //here we cant write swal function as it is asynchr code(which runs always in last) and our page is loading automatically page loading prevent running of async code that's why simple alert here
     })
     .catch(e => {
         swal.fire({
